b33fe915ca74fa94949011a364c653868d4bfdb9
Mark Fisher <mark.aaron.fisher@gmail.com>
2017-06-12 18:57:34 -0700

    add a second layout

--- app/src/main/java/com/example/guest/swipeviewtest/PageFragment.java
+++ app/src/main/java/com/example/guest/swipeviewtest/PageFragment.java
@@ -23,10 +23,16 @@ public class PageFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.page_fragment_layout, container, false);
-        textView = (TextView) view.findViewById(R.id.textView);
         Bundle bundle = getArguments();
+        View view;
         String message = Integer.toString(bundle.getInt("count"));
+        if(message.equals("2")){
+            view = inflater.inflate(R.layout.second_layout, container, false);
+        } else{
+            view = inflater.inflate(R.layout.page_fragment_layout, container, false);
+        }
+        textView = (TextView) view.findViewById(R.id.textView);
+
         textView.setText("This is the " + message +"th page");
         // Inflate the layout for this fragment
         return view;
