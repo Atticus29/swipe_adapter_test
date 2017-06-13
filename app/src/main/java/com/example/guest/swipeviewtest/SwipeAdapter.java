8147b784aced75e8630110b43d8330d1e8c9ffe7
Mark Fisher <mark.aaron.fisher@gmail.com>
2017-06-12 18:49:54 -0700

    first commit

---
+++ app/src/main/java/com/example/guest/swipeviewtest/SwipeAdapter.java
@@ -0,0 +1,31 @@
+package com.example.guest.swipeviewtest;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentStatePagerAdapter;
+
+/**
+ * Created by Guest on 6/12/17.
+ */
+
+public class SwipeAdapter extends FragmentStatePagerAdapter{
+
+    public SwipeAdapter(FragmentManager fm){
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        Fragment fragment = new PageFragment();
+        Bundle bundle = new Bundle();
+        bundle.putInt("count", position+1);
+        fragment.setArguments(bundle);
+        return fragment;
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+}
