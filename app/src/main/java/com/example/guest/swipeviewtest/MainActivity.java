e3f1c5ca1c83e21683c947586200b7a2916e04be
Janek Brandt and Mark Fisher <mark@>
2017-06-15 08:34:42 -0700

    add functioning example of swipe layout

--- app/src/main/java/com/example/guest/swipeviewtest/MainActivity.java
+++ app/src/main/java/com/example/guest/swipeviewtest/MainActivity.java
@@ -4,18 +4,57 @@ import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+
+import com.daimajia.swipe.SwipeLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends FragmentActivity {
-
-    ViewPager viewPager;
+    @Bind(R.id.swipeLayout) SwipeLayout swipeLayout;
+    @Bind(R.id.bottom_wrapper) LinearLayout bottomLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        viewPager = (ViewPager) findViewById(R.id.view_pager);
-        SwipeAdapter swipeAdapter = new SwipeAdapter(getSupportFragmentManager());
-        viewPager.setAdapter(swipeAdapter);
+        ButterKnife.bind(this);
+        swipeLayout.setShowMode(SwipeLayout.ShowMode.LayDown);
+//        swipeLayout.addDrag(SwipeLayout.DragEdge.Left, bottomLayout);
+
+
+        swipeLayout.addSwipeListener(new SwipeLayout.SwipeListener() {
+            @Override
+            public void onClose(SwipeLayout layout) {
+                //when the SurfaceView totally cover the BottomView.
+            }
+
+            @Override
+            public void onUpdate(SwipeLayout layout, int leftOffset, int topOffset) {
+                //you are swiping.
+            }
+
+            @Override
+            public void onStartOpen(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onOpen(SwipeLayout layout) {
+                //when the BottomView totally show.
+            }
+
+            @Override
+            public void onStartClose(SwipeLayout layout) {
+
+            }
+
+            @Override
+            public void onHandRelease(SwipeLayout layout, float xvel, float yvel) {
+                //when user's hand released.
+            }
+        });
 
     }
 }
